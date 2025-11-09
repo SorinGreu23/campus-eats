d453bffe84bc7c54e071cd62cc99869bd19153b3
Tzutzu <alexandru.apostol4321@gmail.com>
2025-11-10 00:35:11 +0200

    Update password validation rules to enforce complexity requirements

--- src/CampusEats.Api/Features/Users/Create/CreateUserValidator.cs
+++ src/CampusEats.Api/Features/Users/Create/CreateUserValidator.cs
@@ -13,8 +13,8 @@ public class CreateUserValidator : AbstractValidator<CreateUserRequest>
 
         RuleFor(x => x.Password)
             .NotEmpty().WithMessage("Password is required")
-            .MinimumLength(6).WithMessage("Password must be at least 6 characters")
-            .MaximumLength(100);
+            .Matches(@"^(?=.{8,100}$)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).*$")
+            .WithMessage("Password must be 8-100 characters and contain at least one lowercase letter, one uppercase letter, one number and one special character");
 
         RuleFor(x => x.FirstName)
             .NotEmpty().WithMessage("First name is required")
