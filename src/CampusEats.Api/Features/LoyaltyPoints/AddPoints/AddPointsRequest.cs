59661c2ba6a7e362895b6c01cd79c2e78bd3fabc
Tzutzu <alexandru.apostol4321@gmail.com>
2025-11-24 09:20:23 +0200

    Clean up

--- src/CampusEats.Api/Features/LoyaltyPoints/AddPoints/AddPointsRequest.cs
+++ src/CampusEats.Api/Features/LoyaltyPoints/AddPoints/AddPointsRequest.cs
@@ -6,31 +6,23 @@ namespace CampusEats.Api.Features.LoyaltyPoints.AddPoints;
 
 public class AddPointsRequest : IRequest<IResult>
 {
-    [JsonPropertyName("userId")]
     public string UserId { get; set; } = null!;
     
-    [JsonPropertyName("points")]
     public int Points { get; set; }
     
-    [JsonPropertyName("reason")]
     public string? Reason { get; set; }
 }
 
 public class AddPointsResponse
 {
-    [JsonPropertyName("accountId")]
     public Guid AccountId { get; set; }
     
-    [JsonPropertyName("pointsAdded")]
     public int PointsAdded { get; set; }
     
-    [JsonPropertyName("newPointsBalance")]
     public int NewPointsBalance { get; set; }
     
-    [JsonPropertyName("lifetimePoints")]
     public int LifetimePoints { get; set; }
-    
-    [JsonPropertyName("tier")]
+
     public string Tier { get; set; } = null!;
 }
 
